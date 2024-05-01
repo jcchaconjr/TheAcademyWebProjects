317cf714745092ffbabf37f0156072bf2a616f7b
Julio C. Chacon Jr. <j_c_chacon_jr@hotmail.com>
2024-05-01 13:11:54 -0400

    JS cleanup
    - Now that he code works, I cleaned up the code structure a little bit.

--- WebProjects/MockDisneyStore/scripts/script.js
+++ WebProjects/MockDisneyStore/scripts/script.js
@@ -1,14 +1,10 @@
 
-
+let characters = ["Mickey Mouse", "Minnie Mouse", "Goofy", "Donald Duck", "Pluto"];
 
 function loginClick() {
 
-/*     let characters = ["Mickey Mouse", "Minnie Mouse", "Goofy", "Donald Duck", "Pluto"];
-    let charHeader = document.getElementById("header-fill");
-    let charTable = document.getElementById("table-fill"); */
-
-
     const username = document.getElementById("uname");
+    
     document.getElementById("user-string").innerHTML = "Hi, " + username.value + "!";
     document.getElementById("uname").value = "";
     document.getElementById("pword").value = "";
@@ -16,13 +12,16 @@ function loginClick() {
 }
 
 function loadTable() {
-    let characters = ["Mickey Mouse", "Minnie Mouse", "Goofy", "Donald Duck", "Pluto"];
+
     let charTable = document.getElementById("div-load");
+    let fillTable = document.getElementById("table-fill");
+
     for (let i = 0; i < characters.length; i++) {
         let charRank = i + 1;
         let	tr = "<tr><td>" + characters[i] + "</td><td style=\'text-align:center;\'>" + charRank + "</td></tr>";
-        document.getElementById("table-fill").innerHTML += tr;
+        fillTable.innerHTML += tr;
     }
+
     if (characters.length > 0) {
         charTable.style["display"] = "block";
     }
