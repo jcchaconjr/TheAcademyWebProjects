74b8118f46e513531a10a1d910908b8ab263c559
Julio C. Chacon Jr. <j_c_chacon_jr@hotmail.com>
2024-04-30 22:22:30 -0400

    Changes based on class work
    - Updated the script.js file with content worked on for class.
    - Updated the stylesheet inpreparaton to dynamically populate a 2nd table with a character list.
    - Updated the shopping page for compatibility with the items in progress.

--- WebProjects/MockDisneyStore/scripts/script.js
+++ WebProjects/MockDisneyStore/scripts/script.js
@@ -1,7 +1,15 @@
-function onPageLoad() {
 
+let characters = ["Mickey Mouse", "Minnie Mouse", "Goofy", "Donald Duck", "Pluto"];
+let charHeader = document.getElementById("header-fill");
+let charTable = document.getElementById("table-fill");
+
+
+function loginClick() {
+
+    const username = document.getElementById("uname");
+    document.getElementById("user-string").innerHTML = "Hi, " + username.value + "!";
+    document.getElementById("uname").value = "";
+    document.getElementById("pword").value = "";
+    location.href = "#";
 }
 
-function onLoginClick() {
-
-}
