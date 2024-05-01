61e164fd761ec0ff15424b74fa1f262b381d1f43
Julio C. Chacon Jr. <j_c_chacon_jr@hotmail.com>
2024-05-01 14:32:13 -0400

    Small Optmization
    - Made the array global, in case I want to use it for other functions.

--- WebProjects/MockDisneyStore/scripts/script.js
+++ WebProjects/MockDisneyStore/scripts/script.js
@@ -1,10 +1,10 @@
 
-let characters = ["Mickey Mouse", "Minnie Mouse", "Goofy", "Donald Duck", "Pluto"];
+let characters = [ "Mickey Mouse", "Minnie Mouse", "Goofy", "Donald Duck", "Pluto"];
 
 function loginClick() {
 
     const username = document.getElementById("uname");
-    
+
     document.getElementById("user-string").innerHTML = "Hi, " + username.value + "!";
     document.getElementById("uname").value = "";
     document.getElementById("pword").value = "";
