72d0032d80fde0e0e214fd1dc1d66393abde4a08
Julio C. Chacon Jr. <j_c_chacon_jr@hotmail.com>
2024-05-01 12:57:14 -0400

    Script adjustments
    Finally fixed all the issues I had with my scripts; the JS functions are now all in the script.js file and are called on page load and the login button press, accorodingly.
    - tableLoad() dynamically loads a character sales table, which is hidden unless it is populated.
    - loginClick takes the username that is input into the username field of the login page and includes the username in the greeting that is located in the header.

--- WebProjects/MockDisneyStore/scripts/script.js
+++ WebProjects/MockDisneyStore/scripts/script.js
@@ -1,15 +1,30 @@
 
-let characters = ["Mickey Mouse", "Minnie Mouse", "Goofy", "Donald Duck", "Pluto"];
-let charHeader = document.getElementById("header-fill");
-let charTable = document.getElementById("table-fill");
 
 
 function loginClick() {
+
+/*     let characters = ["Mickey Mouse", "Minnie Mouse", "Goofy", "Donald Duck", "Pluto"];
+    let charHeader = document.getElementById("header-fill");
+    let charTable = document.getElementById("table-fill"); */
+
 
     const username = document.getElementById("uname");
     document.getElementById("user-string").innerHTML = "Hi, " + username.value + "!";
     document.getElementById("uname").value = "";
     document.getElementById("pword").value = "";
     location.href = "#";
+}
+
+function loadTable() {
+    let characters = ["Mickey Mouse", "Minnie Mouse", "Goofy", "Donald Duck", "Pluto"];
+    let charTable = document.getElementById("div-load");
+    for (let i = 0; i < characters.length; i++) {
+        let charRank = i + 1;
+        let	tr = "<tr><td>" + characters[i] + "</td><td style=\'text-align:center;\'>" + charRank + "</td></tr>";
+        document.getElementById("table-fill").innerHTML += tr;
+    }
+    if (characters.length > 0) {
+        charTable.style["display"] = "block";
+    }
 }
 
