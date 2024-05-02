c73462fa938032e674997b02e53284f17fe7b158
Julio C. Chacon Jr. <j_c_chacon_jr@hotmail.com>
2024-05-02 12:40:54 -0400

    Inventory Improvements
    - Expanded on the functions example and fixed a few lingering bugs discovered during implementation in ProductList.js.
    - Converted the Array elements int eh Prduct List object to be represented as list catagories; changed the form elements to dropdowns on the InventoryList.html page.

--- WebProjects/MockDisneyStore/scripts/ProductList.js
+++ WebProjects/MockDisneyStore/scripts/ProductList.js
@@ -1,50 +1,26 @@
 class Product {
     constructor(productName, category, characterName, franchise, show) {
-      this._productName = productName;
-      this._category = new Array(category);
-      this._characterName = characterName;
-      this._franchise = franchise;
-      this._show = new Array (show);
-      this._inventoryCount = 0;
-
-    }
-
-    productName(product) {
-        return this._productName;
-    }
-
-    category() {
-        return this._category;
+      this.productName = productName;
+      this.category = new Array(category);
+      this.characterName = characterName;
+      this.franchise = franchise;
+      this.show = new Array(show);
+      this.inventoryCount = 0;
     }
 
     addCategory(newCategory) {
         this._category.push(newCategory);
     }
 
-    characterName() {
-        return this._characterName;
-    }
-
-    franchise() {
-      return this._franchise;
-    }
-
-    show() {
-        return this._show;
-    }
-
     addShow(show) {
         this._show.push(show)
     }
 
-    inventoryCount() {
-        return this._inventoryCount;
-    }
 }
 
 const ProductList = new Array;
 
-// gets a product fromt eh lsit when you specify the Product Name
+// gets a product fromt eh lsit when you specify the Product Name...
 function getProduct(productName) {
     return ProductList.find((Product) => Product.productName === productName );
 }
@@ -56,12 +32,35 @@ function addProduct(product, catgory, characterName, franchise, show) {
         let newProduct = new Product(product, catgory, characterName, franchise, show);
         ProductList.push(newProduct);
         console.log("product successfully added to Product List");
+        // Let's refresh the table;
+        populateTable(ProductList);
         document.getElementById("resultMsg").innerHTML = "Product successfully added to Product List!"
         document.getElementById("invform").reset();
         return true;
     }
     catch {
         console.log("There was a problem adding the Product to the Product List. Please check that all entry items were correct.");
+        return false;
+    }
+}
+
+// This will build the code snippet to add to the Product List table... 
+function populateTable(productList) {
+
+    let tableStr = `<tr>\n<th>Product Name</th>\n<th>Category</th>\n<th>Character Name</th>\n<th>Franchise</th>\n<th>Movie/Show</th>\n</tr>`;
+
+    try {
+        for (let product of productList) {
+            tableStr += "<tr><td>" + product.productName + "</td><td>" + product.category + "</td><td>" + product.characterName +
+              "</td><td>" + product.franchise + "</td><td>" + product.show + "</td></tr>";
+              console.log("Adding row to tableStr.");
+        }
+        document.getElementById("prod-fill").innerHTML = tableStr;
+        console.log("Updated table!");
+        return true;
+    }
+    catch {
+        console.log("There was a problem populating the table.");
         return false;
     }
 }
